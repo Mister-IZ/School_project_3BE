dc4f076fbacc2b1f013c807528169b8af10df886
Mister-I.Z <20057@ecam.be>
2023-12-18 21:34:00 +0100

    Pour l'option : Création du bouton + Text couleur

--- School/ColorPage.xaml.cs
+++ School/ColorPage.xaml.cs
@@ -9,6 +9,12 @@ public partial class ColorPage : ContentPage
 		BindingContext = new ColorViewModel();
 	}
 
+    private void OnReturnToMainPageClicked(object sender, EventArgs e)
+    {
+
+        Navigation.PushAsync(new MainPage());
+    }
+
 	
 }
 
--- School/MainPage.xaml.cs
+++ School/MainPage.xaml.cs
@@ -4,23 +4,10 @@ namespace School
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
         private void OnTeacherPageButtonClicked(object sender, EventArgs e)
@@ -33,16 +20,20 @@ namespace School
 
 			private void OnActivityPageButtonClicked(object sender, EventArgs e)
         {
-            // Ajoutez le code ici pour gérer le clic sur le bouton "Créer Enseignant"
-            // Par exemple, vous pouvez naviguer vers la page des enseignants comme vous l'avez fait pour le bouton Counter.
+
             Navigation.PushAsync(new ActivityPage2());
         }
 
 		    private void OnStudentPageButtonClicked(object sender, EventArgs e)
         {
-            // Ajoutez le code ici pour gérer le clic sur le bouton "Créer Enseignant"
-            // Par exemple, vous pouvez naviguer vers la page des enseignants comme vous l'avez fait pour le bouton Counter.
+
             Navigation.PushAsync(new StudentPage2());
+        }
+
+        	private void OnColorPageButtonClicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new ColorPage());
         }
     }
 }
