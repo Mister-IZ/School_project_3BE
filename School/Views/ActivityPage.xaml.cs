70557066c6b43635dd54626082f1989259a2c68f
Mister-I.Z <20057@ecam.be>
2023-12-20 19:47:45 +0100

    Lien Enseignant/Activité + Affichage de la liste des activité

--- School/Views/ActivityPage.xaml.cs
+++ School/Views/ActivityPage.xaml.cs
@@ -2,19 +2,86 @@ namespace School.Views;
 
 public partial class ActivityPage : ContentPage
 {
-	public ActivityPage()
-	{
-		InitializeComponent();
-	}
-
-	    private void OnReturnToMainPageClicked(object sender, EventArgs e)
+    public ActivityPage()
     {
+        InitializeComponent();
 
+        // Chargez et affichez la liste des activités lors de la création de la page
+        LoadActivityList();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Mettez à jour la liste des activités chaque fois que la page devient visible
+        LoadActivityList();
+    }
+
+    // Méthode pour charger et afficher la liste des activités
+    private void LoadActivityList()
+    {
+        try
+        {
+            string filePath = "activitys.txt";
+
+            // Vérifiez si le fichier existe avant de tenter de le lire
+            if (File.Exists(filePath))
+            {
+                // Lisez toutes les lignes du fichier
+                var activityLines = File.ReadAllLines(filePath);
+
+                // Créez une liste d'activités à partir des lignes du fichier
+                var activities = new List<ActivityModel>();
+
+                foreach (var activityLine in activityLines)
+                {
+                    // Divisez chaque ligne pour obtenir les données individuelles (cours, code, ECTS, etc.)
+                    var activityData = activityLine.Split(',');
+
+                    // Assurez-vous que la ligne contient le nombre d'éléments attendu
+                    if (activityData.Length == 4) // Vous avez 4 éléments dans votre modèle ActivityModel
+                    {
+                        // Créez une nouvelle activité à partir des données
+                        var activity = new ActivityModel
+                        {
+                            course = activityData[0].Trim(),
+                            code = activityData[1].Trim(),
+                            ECTS = activityData[2].Trim(),
+                            Teacher = activityData[3].Trim()
+                        };
+
+                        // Ajoutez l'activité à la liste
+                        activities.Add(activity);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"La ligne '{activityLine}' ne contient pas le nombre d'éléments attendu.");
+                    }
+                }
+
+                // Liez la liste des activités au ListView
+                ActivityListView.ItemsSource = activities;
+            }
+            else
+            {
+                // Gérez le cas où le fichier n'existe pas
+            }
+        }
+        catch (Exception ex)
+        {
+            // Gérez les erreurs lors de la lecture du fichier ici
+            Console.WriteLine($"Erreur lors de la lecture du fichier activitys.txt : {ex.Message}");
+        }
+    }
+
+    private void OnReturnToMainPageClicked(object sender, EventArgs e)
+    {
         Navigation.PushAsync(new MainPage());
     }
-		private void OnActivityPageButtonClicked(object sender, EventArgs e)
-	{
 
-		Navigation.PushAsync(new ActivityPage2());
-	}
+    private void OnActivityPageButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ActivityPage2());
+    }
 }
--- School/Views/ActivityPage2.xaml.cs
+++ School/Views/ActivityPage2.xaml.cs
@@ -2,32 +2,85 @@ namespace School.Views;
 
 public partial class ActivityPage2 : ContentPage
 {
-	public ActivityPage2()
-	{
-		InitializeComponent();
-	}
+    // Ajoutez un champ pour stocker la liste des enseignants
+    private List<string> teacherList;
 
-
-	private void OnCreateActivityClicked(object sender, EventArgs e)
+    public ActivityPage2()
     {
-        var activityModel = new ActivityModel
-		{
-                course = courseEntered.Text,
-                code = courseCode.Text,
-                ECTS = courseCredits.Text,
-                Teacher = courseTeacher.Text,
-        };
+        InitializeComponent();
 
-            var activityRepository = new ActivityRepository();
-            activityRepository.SaveActivitys(activityModel);
-
-		Navigation.PushAsync(new MainPage());
+        // Appelez la méthode pour charger les enseignants dans le Picker
+        LoadTeachersPicker();
     }
 
-        private void OnReturnToMainPageClicked(object sender, EventArgs e)
+    // Méthode pour charger les enseignants dans le Picker
+    private void LoadTeachersPicker()
     {
+        try
+        {
+            string filePath = "teachers.txt";
+
+            // Vérifiez si le fichier existe avant de tenter de le lire
+            if (File.Exists(filePath))
+            {
+                // Lisez toutes les lignes du fichier
+                var teacherLines = File.ReadAllLines(filePath);
+
+                // Initialisez la liste des enseignants
+                teacherList = new List<string>();
+
+                foreach (var teacherLine in teacherLines)
+                {
+                    // Divisez chaque ligne pour obtenir les données individuelles (nom, prénom, salaire, etc.)
+                    var teacherData = teacherLine.Split(',');
+
+                    // Assurez-vous que la ligne contient le nombre d'éléments attendu
+                    if (teacherData.Length == 3)
+                    {
+                        // Ajoutez le nom complet de l'enseignant à la liste
+                        var fullName = $"{teacherData[0].Trim()} {teacherData[1].Trim()}";
+                        teacherList.Add(fullName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"La ligne '{teacherLine}' ne contient pas le nombre d'éléments attendu.");
+                    }
+                }
+
+                // Liez la liste des enseignants au Picker
+                courseTeacherPicker.ItemsSource = teacherList;
+            }
+            else
+            {
+                // Gérez le cas où le fichier n'existe pas
+            }
+        }
+        catch (Exception ex)
+        {
+            // Gérez les erreurs lors de la lecture du fichier ici
+            Console.WriteLine($"Erreur lors de la lecture du fichier teachers.txt : {ex.Message}");
+        }
+    }
+
+    private void OnCreateActivityClicked(object sender, EventArgs e)
+    {
+        var activityModel = new ActivityModel
+        {
+            course = courseEntered.Text,
+            code = courseCode.Text,
+            ECTS = courseCredits.Text,
+            // Utilisez la sélection du Picker pour le nom de l'enseignant
+            Teacher = courseTeacherPicker.SelectedItem?.ToString() ?? string.Empty,
+        };
+
+        var activityRepository = new ActivityRepository();
+        activityRepository.SaveActivitys(activityModel);
 
         Navigation.PushAsync(new MainPage());
     }
+
+    private void OnReturnToMainPageClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage());
+    }
 }
-	
